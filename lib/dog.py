d1f7adaa777f0e1a79d87be9ac74b074b5b3fad0
naisiae.kasura@gmail.com <imani.kasura@student.moringaschool.com>
2023-12-16 22:44:17 +0300

    completed assignment

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,26 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name=None, breed=None):
+        self._name = None
+        self._breed = None
+        self.name = name
+        self.breed = breed
+
+    def name(self):
+        return self._name
+
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+
+    def breed(self):
+        return self._breed
+
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in the list of approved breeds.")
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,26 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None):
+        self._name = None
+        self._job = None
+        self.name = name
+        self.job = job
+
+    def name(self):
+        return self._name
+
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()
+        else:
+            print("Name must be a string under 25 characters.")
+
+    def job(self):
+        return self._job
+
+    def job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in the list of approved jobs.")
